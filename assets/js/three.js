b8960316187fddd8d38f6acd0d7c3f3b51ef64ab
Tomas <tomas.pinto94mmil@gmail.com>
2022-09-29 08:44:26 -0500

    changed whole thing to follow simondev tutorials

--- assets/js/three.js
+++ assets/js/three.js
@@ -1,87 +1,343 @@
 const THREE = require("three");
+//camera controls
 const {
-  FlyControls,
-} = require("./node_modules/three/examples/js/controls/FlyControls");
-const { boxMesh } = require("./node_modules/my-modules/boxesMesh");
+  OrbitControls,
+} = require("./node_modules/three/examples/js/controls/OrbitControls");
+//models loaders
+const {
+  FBXLoader,
+} = require("./node_modules/three/examples/js/loaders/FBXLoader");
+const {
+  GLTFLoader,
+} = require("./node_modules/three/examples/js/loaders/GLTFLoader");
+//const { boxMesh } = require("./node_modules/my-modules/boxesMesh");//box grid, not box mesh
 
-// init
-
-//camara
-const camera = new THREE.PerspectiveCamera(
-  70,
-  window.innerWidth / window.innerHeight,
-  0.01,
-  100
-);
-camera.position.set(20, -5, 30);
-
-//rotating camera tryouts
-//FlyControls
-controls = new THREE.FlyControls(camera, renderer.domElement);
-controls.movementSpeed = 100;
-controls.rollSpeed = Math.PI / 24;
-controls.autoForward = false;
-controls.dragToLook = true;
-
-//escena
-
-const scene = new THREE.Scene();
-
-//probando boxmesh
-
-let cajaGrilla = boxMesh(4, 4);
-for (let i = 0; i < cajaGrilla.length; i++) {
-  for (let j = 0; j < cajaGrilla[i].length; j++) {
-    scene.add(cajaGrilla[i][j]);
+class BasicCharacterControls {
+  constructor(params) {
+    this._Init(params);
   }
-}
 
-//plano
+  _Init(params) {
+    this._params = params;
+    this._move = {
+      forward: false,
+      backward: false,
+      left: false,
+      right: false,
+    };
+    this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+    this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
+    this._velocity = new THREE.Vector3(0, 0, 0);
 
-const planeGeometry = new THREE.PlaneGeometry(20, 20);
-const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0xffff00,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.position.set(0, 0, 0);
-scene.add(plane);
+    document.addEventListener("keydown", (e) => this._onKeyDown(e), false);
+    document.addEventListener("keyup", (e) => this._onKeyUp(e), false);
+  }
 
-//plano2 para marcar el centro
-const plane2Geometry = new THREE.PlaneGeometry(1, 1);
-const plane2Material = new THREE.MeshPhongMaterial({
-  color: 0x00ffff,
-});
-const plane2 = new THREE.Mesh(plane2Geometry, plane2Material);
-plane2.position.set(1, 1, 1);
-scene.add(plane2);
-
-//luz
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(0, 0, 10);
-scene.add(directionalLight);
-
-//renderer
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-renderer.setAnimationLoop(animation);
-//renderer.render(scene, camera);
-
-document.body.appendChild(renderer.domElement);
-
-// animation
-
-function animation(time) {
-  //rotating cubes; will be deleted
-  for (let i = 0; i < cajaGrilla.length; i++) {
-    for (let j = 0; j < cajaGrilla[i].length; j++) {
-      cajaGrilla[i][j].rotation.x = time / 2000;
-      cajaGrilla[i][j].rotation.y = time / 1000;
+  _onKeyDown(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._move.forward = true;
+        break;
+      case 65: // a
+        this._move.left = true;
+        break;
+      case 83: // s
+        this._move.backward = true;
+        break;
+      case 68: // d
+        this._move.right = true;
+        break;
+      case 38: // up
+      case 37: // left
+      case 40: // down
+      case 39: // right
+        break;
     }
   }
-  //updating camera
-  controls.update(0.01);
-  renderer.render(scene, camera);
+
+  _onKeyUp(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._move.forward = false;
+        break;
+      case 65: // a
+        this._move.left = false;
+        break;
+      case 83: // s
+        this._move.backward = false;
+        break;
+      case 68: // d
+        this._move.right = false;
+        break;
+      case 38: // up
+      case 37: // left
+      case 40: // down
+      case 39: // right
+        break;
+    }
+  }
+
+  Update(timeInSeconds) {
+    const velocity = this._velocity;
+    const frameDecceleration = new THREE.Vector3(
+      velocity.x * this._decceleration.x,
+      velocity.y * this._decceleration.y,
+      velocity.z * this._decceleration.z
+    );
+    frameDecceleration.multiplyScalar(timeInSeconds);
+    frameDecceleration.z =
+      Math.sign(frameDecceleration.z) *
+      Math.min(Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+
+    velocity.add(frameDecceleration);
+
+    const controlObject = this._params.target;
+    const _Q = new THREE.Quaternion();
+    const _A = new THREE.Vector3();
+    const _R = controlObject.quaternion.clone();
+
+    if (this._move.forward) {
+      velocity.z += this._acceleration.z * timeInSeconds;
+    }
+    if (this._move.backward) {
+      velocity.z -= this._acceleration.z * timeInSeconds;
+    }
+    if (this._move.left) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+    if (this._move.right) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, -Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+
+    controlObject.quaternion.copy(_R);
+
+    const oldPosition = new THREE.Vector3();
+    oldPosition.copy(controlObject.position);
+
+    const forward = new THREE.Vector3(0, 0, 1);
+    forward.applyQuaternion(controlObject.quaternion);
+    forward.normalize();
+
+    const sideways = new THREE.Vector3(1, 0, 0);
+    sideways.applyQuaternion(controlObject.quaternion);
+    sideways.normalize();
+
+    sideways.multiplyScalar(velocity.x * timeInSeconds);
+    forward.multiplyScalar(velocity.z * timeInSeconds);
+
+    controlObject.position.add(forward);
+    controlObject.position.add(sideways);
+
+    oldPosition.copy(controlObject.position);
+  }
 }
+
+//ambient like background andd plane
+class MinecraftDemo {
+  constructor() {
+    this._Initialize();
+  }
+
+  // init
+
+  _Initialize() {
+    //renderer
+
+    this._threejs = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this._threejs.domElement);
+
+    window.addEventListener(
+      "resize",
+      () => {
+        this._OnWindowResize();
+      },
+      false
+    );
+
+    //camera
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(75, 20, 0);
+
+    //scene
+
+    this._scene = new THREE.Scene();
+
+    //light
+    //directional light for shadows
+    let light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this._scene.add(light);
+    //ammbient light
+    light = new THREE.AmbientLight(0x404040); //(4.0) como segundo parametro
+    this._scene.add(light);
+
+    //camera controls
+
+    const controls = new THREE.OrbitControls(
+      this._camera,
+      this._threejs.domElement
+    );
+    controls.target.set(0, 20, 0);
+    controls.update();
+
+    //skybox cube map
+
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      "assets/img/cloudy/yellowcloud_rt.jpg",
+      "assets/img/cloudy/yellowcloud_lf.jpg",
+      "assets/img/cloudy/yellowcloud_up.jpg",
+      "assets/img/cloudy/yellowcloud_dn.jpg",
+      "assets/img/cloudy/yellowcloud_bk.jpg",
+      "assets/img/cloudy/yellowcloud_ft.jpg",
+    ]);
+    this._scene.background = texture;
+
+    //plano
+
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(100, 100, 10, 10),
+      new THREE.MeshStandardMaterial({
+        color: 0x202020,
+      })
+    );
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this._scene.add(plane);
+
+    this._mixers = [];
+    this._previousRAF = null;
+
+    //this._LoadAnimatedModel();
+    this._LoadModel();
+
+    this._RAF();
+  }
+  /*
+  _LoadAnimatedModel() {
+    const loader = new THREE.FBXLoader();
+    loader.setPath("./resources/zombie/");
+    loader.load("mremireh_o_desbiens.fbx", (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse((c) => {
+        c.castShadow = true;
+      });
+
+      const params = {
+        target: fbx,
+        camera: this._camera,
+      };
+      this._controls = new BasicCharacterControls(params);
+
+      const anim = new THREE.FBXLoader();
+      anim.setPath("./resources/zombie/");
+      anim.load("walk.fbx", (anim) => {
+        const m = new THREE.AnimationMixer(fbx);
+        this._mixers.push(m);
+        const idle = m.clipAction(anim.animations[0]);
+        idle.play();
+      });
+      this._scene.add(fbx);
+    });
+  }
+
+  _LoadAnimatedModelAndPlay(path, modelFile, animFile, offset) {
+    const loader = new THREE.FBXLoader();
+    loader.setPath(path);
+    loader.load(modelFile, (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse((c) => {
+        c.castShadow = true;
+      });
+      fbx.position.copy(offset);
+
+      const anim = new THREE.FBXLoader();
+      anim.setPath(path);
+      anim.load(animFile, (anim) => {
+        const m = new THREE.AnimationMixer(fbx);
+        this._mixers.push(m);
+        const idle = m.clipAction(anim.animations[0]);
+        idle.play();
+      });
+      this._scene.add(fbx);
+    });
+  }
+  */
+
+  _LoadModel() {
+    const loader = new THREE.GLTFLoader();
+    loader.load(
+      "assets/3d-models/spartan_armour_mkv_-_halo_reach.glb",
+      (gltf) => {
+        gltf.scene.traverse((c) => {
+          c.castShadow = true;
+        });
+        this._scene.add(gltf.scene);
+      }
+    );
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _RAF() {
+    requestAnimationFrame((t) => {
+      if (this._previousRAF === null) {
+        this._previousRAF = t;
+      }
+
+      this._RAF();
+
+      this._threejs.render(this._scene, this._camera);
+      this._Step(t - this._previousRAF);
+      this._previousRAF = t;
+    });
+  }
+
+  _Step(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+    if (this._mixers) {
+      this._mixers.map((m) => m.update(timeElapsedS));
+    }
+
+    if (this._controls) {
+      this._controls.Update(timeElapsedS);
+    }
+  }
+}
+let _APP = null;
+
+window.addEventListener("DOMContentLoaded", () => {
+  _APP = new MinecraftDemo();
+});
